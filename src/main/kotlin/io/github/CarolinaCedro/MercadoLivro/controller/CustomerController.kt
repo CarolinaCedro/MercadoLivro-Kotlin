582c71bdff815185e46d8bf2dce72e036202f860
CarolinaCedro <yotechgirl@gmail.com>
2022-09-12 13:53:08 -0300

    feat: create customer model

--- src/main/kotlin/io/github/CarolinaCedro/MercadoLivro/controller/CustomerController.kt
+++ src/main/kotlin/io/github/CarolinaCedro/MercadoLivro/controller/CustomerController.kt
@@ -1,5 +1,6 @@
 package io.github.CarolinaCedro.MercadoLivro.controller
 
+import io.github.CarolinaCedro.MercadoLivro.model.CustomerModel
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -8,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/")
 class CustomerController {
     @GetMapping
-    fun helloWorld(): String {
-        return "Hello word"
+    fun helloWorld(): CustomerModel {
+        return CustomerModel("1","Ana","ana@email.com")
     }
 
 
---
+++ src/main/kotlin/io/github/CarolinaCedro/MercadoLivro/model/CustomerModel.kt
@@ -0,0 +1,8 @@
+package io.github.CarolinaCedro.MercadoLivro.model
+
+data class CustomerModel(
+    var id: String,
+    var name: String,
+    var email: String
+
+)
