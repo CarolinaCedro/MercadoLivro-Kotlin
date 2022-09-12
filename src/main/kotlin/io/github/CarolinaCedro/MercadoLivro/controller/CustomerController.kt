10080b3a16c88d48fcab831c9fc143f1828c0784
CarolinaCedro <yotechgirl@gmail.com>
2022-09-12 14:13:56 -0300

    feat: add responseStatus in controller req

--- src/main/kotlin/io/github/CarolinaCedro/MercadoLivro/controller/CustomerController.kt
+++ src/main/kotlin/io/github/CarolinaCedro/MercadoLivro/controller/CustomerController.kt
@@ -2,9 +2,12 @@ package io.github.CarolinaCedro.MercadoLivro.controller
 
 import io.github.CarolinaCedro.MercadoLivro.controller.request.PostCustomerRequest
 import io.github.CarolinaCedro.MercadoLivro.model.CustomerModel
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -15,6 +18,8 @@ class CustomerController {
         return CustomerModel("1","Ana","ana@email.com")
     }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody customer:PostCustomerRequest){
         println(customer)
     }
