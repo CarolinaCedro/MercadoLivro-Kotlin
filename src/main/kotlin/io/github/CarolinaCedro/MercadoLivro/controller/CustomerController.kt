2940c8477c41cd999b1b81dfa68938e6401126a9
CarolinaCedro <yotechgirl@gmail.com>
2022-09-12 13:42:43 -0300

    feat: hello world in kotlin

--- src/main/kotlin/io/github/CarolinaCedro/MercadoLivro/controller/CustomerController.kt
+++ src/main/kotlin/io/github/CarolinaCedro/MercadoLivro/controller/CustomerController.kt
@@ -1,4 +1,16 @@
 package io.github.CarolinaCedro.MercadoLivro.controller
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/")
 class CustomerController {
+    @GetMapping
+    fun helloWorld(): String {
+        return "Hello word"
+    }
+
+
 }
